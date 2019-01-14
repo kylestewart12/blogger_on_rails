f394141dd503f6194b7d4277fc7ba4faec60a455
Kyle Stewart <kyle4@vt.edu>
2019-01-13 22:25:15 -0500

    Added authentication feature.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,11 @@
 class CommentsController < ApplicationController
+  before_action :require_login, except: :create 
   def create
     @comment = Comment.new(comment_params)
     @comment.article_id = params[:article_id]
 
+    @comment.save
+    
     redirect_to article_path(@comment.article)
   end
 
